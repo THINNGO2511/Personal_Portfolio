eb636d635632d6f464d86b8556ef778db48bf0fb
Thinh Ngo <ngothinh2511@gmail.com>
2023-12-23 00:55:16 -0600

    added site layout

--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,11 +1,23 @@
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar';
 import './index.scss';
 
 const Layout = () =>{
     return (
-        <>
+        <div className="App">
             <Sidebar/>
-        </>
+            <div className='page'>
+                <span className='tags top-tags'>&lt;Go Coogs!&gt;</span>
+
+                <Outlet />
+
+                <span className='tags bottom-tags'>
+                &lt;Houston, we got a problem!&gt;
+                <br />
+                <span className='bottom-tag-html'>&lt;Dare, Unite, Explore&gt;</span>
+                </span>
+            </div>
+        </div>
     )
 }
 
